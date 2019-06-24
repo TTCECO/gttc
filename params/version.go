648b9f95fcddd339bd645d332e1411c85cab54f2
TATAUFO <liupeng@tataufo.com>
2019-06-24 16:46:43 +0800

    update version to 0.1.8

--- params/version.go
+++ params/version.go
@@ -23,7 +23,7 @@ import (
 const (
 	VersionMajor = 0          // Major version component of the current release
 	VersionMinor = 1          // Minor version component of the current release
-	VersionPatch = 7          // Patch version component of the current release
+	VersionPatch = 8          // Patch version component of the current release
 	VersionMeta  = "unstable" // Version metadata to append to the version string
 )
 
